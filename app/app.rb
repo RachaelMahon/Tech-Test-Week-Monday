42423e8b794154c4c83eee069edc2506afc5adc3
RachaelMahon <rachmahon@gmail.com>
2016-11-28 18:28:45 +0000

    push for end of the day

--- app/app.rb
+++ app/app.rb
@@ -10,23 +10,40 @@ class Monday < Sinatra::Base
     'Happy Tech Test Week!'
   end
 
-  get '/set' do
-    @session_array = Array.new
-    @key = params['somekey']
-    @value = params['somevalue']
-    @session_array << {@key => @value}
-    # @session_array.add_key_value_pair_to_hash(@key, @somevalue)
-    # send key value pair to an array to be stored
-    'Your hash is being stored' + "#{@session_array}"
 
+  get '/set' do
+    session[:params]
+    params.each {|key, value| session[:"#{key}"] = value}
+    erb :setter
   end
+
 
   get '/get' do
-    @key = params[:somekey]
-    @value = @session_array.array.select {|k, v| k = "#{@key}" }
-      # go to array and find the value associated with this key
-
+    p session[:params]
+    p @key = params[:key]
+    p @value = session[:"#{@key}"]
+    p "#{params[:key]}"
   end
+
+  # get '/set' do
+  #   # /set?somekey=somevalue
+  #   @session_array = Array.new
+  #   @key = params[:somekey]
+  #   @value = params[:somevalue]
+  #   @session_array << {@key => @value}
+  #   # @session_array.add_key_value_pair_to_hash(@key, @somevalue)
+  #   # send key value pair to an array to be stored
+  #   'Your hash is being stored' + "#{@session_array}"
+  #
+  # end
+  #
+  # get '/get' do
+  #   # /get?key=somekey
+  #   @key = params[:somekey]
+  #   @value = @session_array.array.select {|k, v| k = "#{@key}" }
+  #     # go to array and find the value associated with this key
+  #
+  # end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
--- app/array.rb
+++ app/array.rb
@@ -2,12 +2,16 @@ class Array_Model
 
 attr_reader :array
 
-  def initialize
-    @array = []
+  class << self
+    attr_accessor :store
   end
 
-  def add_key_value_pair_to_hash(k, v)
-    @array << {k => v}
-  end
+    def initialize
+      @array = []
+    end
 
-end
+    def add_key_value_pair_to_hash(k, v)
+      @array << {k => v}
+    end
+
+  end
